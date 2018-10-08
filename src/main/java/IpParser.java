aaf635ea0f1d1bf2f59e7c7c4b4a1f4411672497
Anatolii Nizkopoklonnyi <a.nizkopoklonnyi@gmail.com>
2018-10-08 19:11:04 +0300

    Create validation for input data

--- src/main/java/IpParser.java
+++ src/main/java/IpParser.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.StringUtils;
+
 import java.util.ArrayList;
 //ArrayList<Integer>
 public class IpParser {
@@ -5,23 +7,33 @@ public class IpParser {
     ArrayList<Integer> IpElements= new ArrayList<Integer>();
 
     public ArrayList<Integer> execute(String ipStr){
+        //убираем мусор
+        IpElements.clear();
 
         String [] element = ipStr.split("\\.");
 
-        System.out.print(" in parser execute" + ipStr+"\n");
+        //Считаем количество точек
+        int countDot = ipStr.split("\\.",-1).length-1;
+        System.out.println("количество точек = " + countDot);
 
-        for (int i=0; i<element.length; i++){
-            try{
-                IpElements.add(Integer.parseInt(element[i]));
-            }
-            catch(NumberFormatException e ){ //в скобках указывается класс конкретной ожидаемой ошибки
-                System.out.print("Ip element "+i+" ="+ element[i]+" novalide \n");
-                IpElements.clear();
-                IpElements.add(-1);
-                break;
+        System.out.print(" in parser execute " + ipStr+"\n");
+        if(countDot==3) {
+            for (int i = 0; i < element.length; i++) {
+                try {
+                    IpElements.add(Integer.parseInt(element[i]));
+                } catch (NumberFormatException e) { //в скобках указывается класс конкретной ожидаемой ошибки
+                    System.out.print("Ip element " + i + " = " + element[i] + " novalide \n");
+                    IpElements.clear();
+                    IpElements.add(-1);
+                    break;
+                }
             }
         }
-
+        else{
+            System.out.print("Количество точекне равно 3 \n");
+            IpElements.clear();
+            IpElements.add(-1);
+        }
         return IpElements;
     }
 }
--- src/main/java/IpValidator.java
+++ src/main/java/IpValidator.java
@@ -2,14 +2,37 @@ import java.util.ArrayList;
 
 public class IpValidator {
 
-    //validate element limit 0, 255
-    public boolean validate(Integer element){
+    IpParser parser= new IpParser();
+    ArrayList<Integer> Iptmp= new ArrayList<Integer>();
+
+
+    //Определяем находится ли элемент 0, 255
+    private boolean validate(Integer element){
         if(element>=0 && element<=255)
             return true;
         else
             return  false;
     }
-    public boolean check(ArrayList<Integer> Ip) {
-        
+
+    //проверяет на количество элементов в предполанаемом ip
+    //проверяет каждый элемент на допустимый диапозон
+    public boolean check(String IpStr) {
+        //парсим ввеженую строку и возвращаем полученный списиок элементов ip
+        Iptmp = parser.execute(IpStr);
+
+        //если первый элемент -1, то строка не валидна
+            if(Iptmp.size()==4) {
+                for (int i = 0; i < Iptmp.size(); i++) {
+                    if (validate(Iptmp.get(i)) == false) {
+                        System.out.print("Элемент " + Iptmp.get(i) + "вне диапозона 0, 255");
+                        return false;
+                    }
+                }
+            }
+            else {
+                System.out.print("Не валидное количество элементов в ip");
+                return false;
+            }
+        return true;
     }
 }
